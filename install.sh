6c086ce26a47d5557b681c89a648783f922007d9
RaphaelTFool <1207975808@qq.com>
2019-06-28 08:17:11 -0400

    make xgrep install easier
    1. add ack script incase of loss
    2. modify xgrep for ack for zsh

--- install.sh
+++ install.sh
@@ -2,7 +2,8 @@
 
 P=`dirname $(readlink -f $0)`
 
-mkdir -p ~/bin
-ln -fs $P/xgrep ~/bin/xgrep
-curl https://beyondgrep.com/ack-2.22-single-file > ~/bin/ack && chmod 0755 ~/bin/ack
-echo ". ~/bin/xgrep" >> ~/.bashrc
+mkdir -p ~/usr/bin
+cp $P/xgrep ~/usr/bin/xgrep
+[[ -f ack ]] && cp $P/ack ~/usr/bin/ack
+[[ ! -f ack ]] && curl https://beyondgrep.com/ack-2.22-single-file > ~/usr/bin/ack && chmod 0755 ~/usr/bin/ack
+echo ". ~/usr/bin/xgrep" >> ~/.bashrc
